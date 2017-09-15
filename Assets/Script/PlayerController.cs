6d89291652389d6f083baf612b0b4b2712895fa4
Jerm <southon55@gmail.com>
2017-09-15 19:36:22 +1200

    Init commit for PixlJam -- Rogue--like framework set up

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float playerSpeed;
+    public float jumpHeight;
+
+
+    private Animator anim;
+
+
+    //ground stuff
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+    private bool doubleJumped;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float absValueY = GetComponent<Rigidbody2D>().velocity.y;
+        if (grounded)
+            doubleJumped = false;
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.localScale = new Vector3(2, 2, 1);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.localScale = new Vector3(-2, 2, 1);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-playerSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        //Crouching
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+
+        }
+
+        //JUMPING
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            jump();
+        }
+        if (Input.GetKey(KeyCode.Space) && !grounded && !doubleJumped)
+        {
+            jump();
+            doubleJumped = true;
+        }
+    }
+    public void jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+
+        //update animation
+        anim.SetBool("Jumping", true);
+    }
+}
