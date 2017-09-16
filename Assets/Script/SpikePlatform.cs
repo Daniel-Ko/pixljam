61791b6e7b19d7b80e36d87e328839d60fddb503
Yi Sian Lim <limyisian@gmail.com>
2017-09-16 16:59:46 +1200

    Boing sound is played when player hits spike

--- Assets/Script/SpikePlatform.cs
+++ Assets/Script/SpikePlatform.cs
@@ -7,6 +7,7 @@ public class SpikePlatform : MonoBehaviour {
 	// Spike at the top or bottom.
 	public bool top;
 	public int bounceBackForce = 200;
+	public AudioClip bounceClip;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,9 @@ public class SpikePlatform : MonoBehaviour {
 	}
 		
 	void OnCollisionEnter2D(Collision2D other) {
+
+		bool collided = false;
+
 		if (other.collider.CompareTag("Bird") ||other.collider.CompareTag("Kiwi")){
 
 			ContactPoint2D contact = other.contacts [0];
@@ -27,23 +31,32 @@ public class SpikePlatform : MonoBehaviour {
 				// Collision is from the top.
 				if (Vector3.Dot (contact.normal, Vector3.down) > 0.5) {
 					other.gameObject.SendMessage ("hurtPlayer", 1);
+					collided = true;
 				}
 			} else {
 				// Collision is from the down.
 				if (Vector3.Dot (contact.normal, Vector3.up) > 0.5) {
 					other.gameObject.SendMessage ("hurtPlayer", 1);
+					collided = true;
 				}
 			}
 
-			// Calculate angle between collision point and player.
-			Vector2 platform = (Vector2) transform.position;
-			Vector2 dir = contact.point - platform;
+			if (collided) {
+				// Calculate angle between collision point and player.
+				Vector2 platform = (Vector2)transform.position;
+				Vector2 dir = contact.point - platform;
 
-			// Get the opposite vector and normalized it.
-			dir = -dir.normalized;
+				// Get the opposite vector and normalized it.
+				dir = -dir.normalized;
 
-			// Player bounce back.
-			other.gameObject.GetComponent<Rigidbody2D>().AddForce (dir * bounceBackForce);
+				// Player bounce back.
+				other.gameObject.GetComponent<Rigidbody2D> ().AddForce (dir * bounceBackForce);
+
+				// Bounce back sound.
+				if (bounceClip) {
+					AudioSource.PlayClipAtPoint (bounceClip, transform.position);
+				}
+			}
 				
 		}
 	}
