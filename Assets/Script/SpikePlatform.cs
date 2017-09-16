142accca647dd0f637ad892d456fd8ff119fac90
Yi Sian Lim <limyisian@gmail.com>
2017-09-16 14:10:00 +1200

    Spike platform now reduces player HP

---
+++ Assets/Script/SpikePlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikePlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+		
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Bird" || other.tag == "Kiwi"){
+			PlayerController script = other.GetComponent<PlayerController>();
+			script.hurtPlayer (1);
+		}
+	}
+		
+}
