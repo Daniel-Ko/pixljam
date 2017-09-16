51370c20757faa8110c7def1f6b237a463682d5d
Dan Ko <kodani@myvuw.ac.nz>
2017-09-17 01:17:44 +1200

    Added the falling linked branch prefabs

---
+++ Assets/Script/Platforms/FallingBranch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBranch : MonoBehaviour {
+	private Rigidbody2D r2d2;
+	public float fallDelay = 2;
+	private FallingLinked parent;
+
+	Vector3 platvec;
+	float w;
+	float h;
+
+	void Start() {
+		platvec = this.transform.position;
+
+		h = GetComponent<SpriteRenderer> ().bounds.size.y;
+
+		r2d2 = GetComponent<Rigidbody2D> ();
+		r2d2.isKinematic = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.collider.CompareTag ("Bird") || coll.collider.CompareTag ("Kiwi")) {
+
+			PlayerController player = coll.gameObject.GetComponent<PlayerController> ();
+
+			if (PlayerIsOnTopOfBranch(player)) {
+				parent = (FallingLinked) transform.parent.gameObject.GetComponent<FallingLinked>();
+				parent.SetFall (true);
+				parent.setPlayer (player);
+//				SetPlatformFallDelay (player); //make the platform fall according to weight of player
+//				StartCoroutine (Fall ()); //make the platform fall
+			}
+		}
+	}
+
+	bool PlayerIsOnTopOfBranch(PlayerController player) {
+		Vector3 pvec = player.gameObject.transform.position;
+
+		return (pvec.y > platvec.y + (h / 3));  //if player is only touching top of object
+	}
+}
---
+++ Assets/Script/Platforms/FallingLinked.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingLinked : MonoBehaviour {
+	private Rigidbody2D r2d2;
+	public float fallDelay = 2;
+	private bool isFalling = false;
+	private PlayerController player;
+
+	Vector3 platvec;
+	float w;
+	float h;
+
+	// Use this for initialization
+	void Start () {
+		r2d2 = GetComponent<Rigidbody2D> ();
+		r2d2.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFalling) {
+			SetPlatformFallDelay (player); //make the platform fall according to weight of player
+			StartCoroutine (Fall ()); //make the platform fall
+		}
+	}
+
+	//called by children
+	public void SetFall(bool canFallNow) {
+		isFalling = canFallNow;
+	}
+
+	//called by player
+	public void setPlayer(PlayerController pc) {
+		if (pc == null)
+			return;
+		player = pc;
+	}
+		
+
+	IEnumerator Fall() {
+		yield return new WaitForSeconds (fallDelay);
+		r2d2.isKinematic = false;
+		GetComponent<PolygonCollider2D> ().isTrigger = true;
+		yield return 0;
+	}
+
+	void SetPlatformFallDelay(PlayerController player) {
+		if (player == null)
+			return;
+
+		float newDelay = 2;
+
+		newDelay -= (float) ((player.weight() / 5) * 0.2);
+		fallDelay = newDelay;
+	}
+}
+
+
