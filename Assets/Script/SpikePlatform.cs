3ea2403ce92737d0f3871b129827243202f9d2cc
Yi Sian Lim <limyisian@gmail.com>
2017-09-16 15:51:43 +1200

    Spike platform now only hurts player where the spike is

--- Assets/Script/SpikePlatform.cs
+++ Assets/Script/SpikePlatform.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SpikePlatform : MonoBehaviour {
 
+	// Spike at the top or bottom.
+	public bool top;
+
 	// Use this for initialization
 	void Start () {
 
@@ -14,10 +17,23 @@ public class SpikePlatform : MonoBehaviour {
 
 	}
 		
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Bird" || other.tag == "Kiwi"){
-			PlayerController script = other.GetComponent<PlayerController>();
-			script.hurtPlayer (1);
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.collider.CompareTag("Bird") ||other.collider.CompareTag("Kiwi")){
+
+			ContactPoint2D contact = other.contacts [0];
+			// top is true if the spike is at the top.
+			if (top) {
+				// Collision is from the top.
+				if (Vector3.Dot (contact.normal, Vector3.down) > 0.5) {
+					other.gameObject.SendMessage ("hurtPlayer", 1);
+				}
+			} else {
+				// Collision is from the down.
+				if (Vector3.Dot (contact.normal, Vector3.up) > 0.5) {
+					other.gameObject.SendMessage ("hurtPlayer", 1);
+				}
+			}
+				
 		}
 	}
 		
